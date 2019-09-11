f6a4ad7e990ac268445c9ca65e6f1f4922e4bbc5
byronwong <5236867+byronwong@users.noreply.github.com>
2019-09-11 17:34:37 +0100

    added Dans changes to index.js

---
+++ src/components/contentCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import contentCardStyles from './contentCard.scss';
+
+const ContentCard = () => {
+
+    return (
+        <div className={contentCardStyles.container}>
+            <h1>Card Title</h1>
+        </div>
+    );
+
+
+};
+
+export default ContentCard;
