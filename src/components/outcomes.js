55285be59d354ea5f1ea75674a7ee9bf0c5b2b3f
Dan Bull <daniel.bull@wipro.com>
2019-09-11 15:47:52 +0100

    changing comment

--- src/components/outcomes.js
+++ src/components/outcomes.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Outcomes = () => (
   <section className="grav-o-full-bleed__content grav-u-pt-xxl grav-u-pb-xxl">
-    {/* PHASES CONTENT HERE */}
+    {/* OUTCOMES CONTENT HERE */}
   </section>
 );
 
