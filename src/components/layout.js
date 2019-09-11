d79f8c8d8e04b2a777dc4bc9b5c1dd15521a94de
Dan Bull <daniel.bull@wipro.com>
2019-09-11 15:45:48 +0100

    Fresh start with key components

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
-import Header from "./header"
-import Footer from "./footer"
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
+import Header from "./header";
+import Footer from "./footer";
+import Helmet from "react-helmet";
 import("../styles/index.scss");
 
 const Layout = ({ children }) => (
@@ -26,21 +26,21 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-      <Helmet
-    bodyAttributes={{
-        class: 'grav-o-full-bleed'
-    }}
-/>
+        <Helmet
+          bodyAttributes={{
+            class: "grav-o-full-bleed"
+          }}
+        />
         <Header siteTitle={data.site.siteMetadata.title} />
         <main>{children}</main>
         <Footer />
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
