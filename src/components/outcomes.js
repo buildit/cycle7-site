c4c7bf44da823ee1911445beff1ca5ab19a66e6b
Dan Bull <daniel.bull@wipro.com>
2019-09-11 15:45:48 +0100

    Fresh start with key components

---
+++ src/components/outcomes.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Outcomes = () => (
+  <section className="grav-o-full-bleed__content grav-u-pt-xxl grav-u-pb-xxl">
+    {/* PHASES CONTENT HERE */}
+  </section>
+);
+
+export default Outcomes;
---
+++ src/components/phases.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Phases = () => (
+  <section className="grav-o-full-bleed__content grav-u-pt-xxl grav-u-pb-xxl">
+    {/* PHASES CONTENT HERE */}
+  </section>
+);
+
+export default Phases;
