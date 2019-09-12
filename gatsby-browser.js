797050386369c2b7e2d43bbb3f192efc40935bf7
byronwong <5236867+byronwong@users.noreply.github.com>
2019-09-12 13:53:58 +0100

    fixed css importing

---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+// import "./src/styles/gravity-overrides.scss";
+// import "./src/styles/cycle7-theme.scss";
+import "./src/styles/index.scss";
+// import "./src/styles/layout.scss"
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { StaticQuery, graphql } from "gatsby";
 import Header from "./header";
 import Footer from "./footer";
 import Helmet from "react-helmet";
-import("../styles/index.scss");
+// import("../styles/index.scss");
 
 const Layout = ({ children }) => (
   <StaticQuery
