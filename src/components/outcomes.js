53e079de4a8ad1b22587247aa643abc71765fbf8
patrick <no@thanks.com>
2019-09-17 15:08:17 +0100

    Outcome component
    Module and styles

--- src/components/outcomes.js
+++ src/components/outcomes.js
@@ -1,19 +1,41 @@
 import React from "react";
+import Tick from "../images/tick.svg"
 
 const Outcomes = () => (
   <section className="grav-o-full-bleed__content grav-u-pt-xxl grav-u-pb-xxl">
     <h2 className="grav-u-text-centered">Success</h2>
     <p>
-      The outcome from 7 weeks intensive effort not only yeilds a viable and tested direction 
+      The outcome from 7 weeks intensive effort yields not only results critical to end users but also discover costly issues before they become assumptions to success. Cycle 7 tests the big idea by exposing the questions that matter and focus on user value.  
     </p>
-    <div className="container">
-      <div className="row">
-        <div className="col">Validated findings through hypothesis driven experiments</div>
-        <div className="col">Fast and iterative prototyping, testing and learning</div>
-        <div className="col">Quick returns offer pivot points and quickly uncover no-gos</div>
-      </div>
-    </div>
+    <ul className="row outcome-list">
+      <li className="col-md-4 outcome-list-item">
+        <div class="media">
+          <img src={Tick} alt="" className="outcome-tick-icon"/>
+          <div>
+            <p>Validated findings through hypothesis driven experiments</p>
+          </div>
+        </div>
+      </li>
+      <li className="col-md-4 outcome-list-item">
+        <div class="media">
+          <img src={Tick} alt="" className="outcome-tick-icon"/>
+          <div>
+            <p>Fastest possible prototyping, testing and learning</p>
+          </div>
+        </div>
+      </li>
+      <li className="col-md-4 outcome-list-item">
+        <div class="media">
+          <img src={Tick} alt="" className="outcome-tick-icon"/>
+          <div>
+            <p>Quick returns find insight, pivot points and uncover no-gos</p>
+          </div>
+        </div>
+      </li>
+    </ul>
   </section>
 );
 
 export default Outcomes;
+
+
