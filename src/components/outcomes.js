a639a875d555240648e127c402cc2855d733e260
patrick <no@thanks.com>
2019-09-17 11:44:56 +0100

    Setup basic component
    Add basic copy and layout

--- src/components/outcomes.js
+++ src/components/outcomes.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const Outcomes = () => (
   <section className="grav-o-full-bleed__content grav-u-pt-xxl grav-u-pb-xxl">
-    {/* OUTCOMES CONTENT HERE */}
+    <h2 className="grav-u-text-centered">Success</h2>
+    <p>
+      The outcome from 7 weeks intensive effort not only yeilds a viable and tested direction 
+    </p>
+    <div className="container">
+      <div className="row">
+        <div className="col">Validated findings through hypothesis driven experiments</div>
+        <div className="col">Fast and iterative prototyping, testing and learning</div>
+        <div className="col">Quick returns offer pivot points and quickly uncover no-gos</div>
+      </div>
+    </div>
   </section>
 );
 
