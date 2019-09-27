d73a08520ef15c4e5369913a437de10c444ba9b7
patrick <no@thanks.com>
2019-09-27 11:19:58 +0100

    update copy

--- src/components/strategy.js
+++ src/components/strategy.js
@@ -4,20 +4,20 @@ const Strategy = () => (
   <section className="strategy">
     <div className="grav-o-full-bleed__content grav-u-pt-xxl grav-u-pb-xxl">
       <div className="grav-u-text-centered">
-        <h3>deliverables - what does it do</h3>
+        <h3>How Cycle 7 Delivers</h3>
       </div>
         <ul className="row strategy-list">
           <li className="col-md-4 strategy-list-item">
             <h4 className="grav-u-font-size-plus-1">Measurable results</h4>
-            <p>Accelerate from qualitative to quantitative data to build real propositions</p>
+            <p>Accelerate from qualitative to quantitative data to build real propositions.</p>
           </li>
           <li className="col-md-4 strategy-list-item">
             <h4 className="grav-u-font-size-plus-1">Unique insights</h4>
-            <p>Stakeholders gain a strategic vantage point to see new opportunities</p>
+            <p>Stakeholders gain a strategic vantage point to see new opportunities.</p>
           </li> 
           <li className="col-md-4 strategy-list-item">
-            <h4 className="grav-u-font-size-plus-1">Always 7 weeks</h4> <br />
-            <p>Our fixed time frame delivers on a fixed budget</p>
+            <h4 className="grav-u-font-size-plus-1">Always 7 weeks</h4>
+            <p>A fixed time frame will deliver answers on schedule, on spec and on budget.</p>
           </li>
         </ul>
     </div>
