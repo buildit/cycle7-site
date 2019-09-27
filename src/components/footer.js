ac8fa0555f699049876c213bd92b648370c635b3
Dan Bull <daniel.bull@wipro.com>
2019-09-27 14:55:16 +0100

    Improved footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,10 @@
 import React from "react";
+import BuilditLogoSvg from "./builditLogoSvg";
 
 const Footer = () => (
-  <footer className="grav-c-page-footer">
-    <div className="grav-o-container">
-      <svg
-        role="img"
-        className="grav-c-logo"
-        aria-labelledby="buildit-logotype__title"
-        width="300"
-        height="33"
-      >
-        <use xlinkHref="#buildit-logotype"></use>
-      </svg>
+  <footer className="grav-c-page-footer grav-u-pt-xl">
+    <div>
+      <p className="c7-footer-credit">Proudly produced by <br /><BuilditLogoSvg /></p>
       <p>
         © 2019 <strong>Buildit</strong>. All rights reserved.
       </p>
