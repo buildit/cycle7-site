b77ffafdd4782aa8c65c1d72e35954ddc993ed5d
Dan Bull <daniel.bull@wipro.com>
2019-11-01 11:21:45 +0000

    WIP

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,94 @@
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import React from "react";
+import { Link } from "react-scroll";
 
-const Header = ({ siteTitle }) => (
-  <header>
-    
-  </header>
-);
+class Header extends Component {
+  handleSetActive = to => {
+    console.log(to);
+  };
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: ``,
-};
+  render() {
+    return (
+      <header className="grav-c-page-header grav-c-page-header--sticky">
+        <div>
+          <div class="grav-c-page-header__logo">
+            LOGO?
+          </div>
+          <button class="grav-c-toggle-menu" type="button" aria-pressed="false" aria-label="Toggle navigation menu">
+            <span class="grav-c-toggle-menu__icon"></span>
+          </button>
+          <nav class="grav-c-nav-menu">
+            <ul>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="outcomes"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  Outcomes
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="strategy"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  Strategy
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="team"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  The team
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="process"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  The process
+                </Link>
+              </li>
+              <li className="grav-c-nav-menu__hero">
+                <Link
+                  activeClass="active"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={500}
+                  onSetActive={this.handleSetActive}
+                >
+                  Get in touch
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    )};      
+}
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "../components/layout";
 import Team from "../components/team";
 import Hero from "../components/hero";
@@ -6,16 +6,51 @@ import Phases from "../components/phases";
 import Outcomes from "../components/outcomes";
 import Strategy from "../components/strategy";
 import Contact from "../components/contact";
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from "react-scroll";
 
-const IndexPage = () => (
-  <Layout>
-    <Hero />
-    <Outcomes />
-    <Strategy />
-    <Team />
-    <Phases />
-    <Contact />
-  </Layout>
-);
+class IndexPage extends Component {
+  componentDidMount() {
+    scrollSpy.update();
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Hero />
+        <Element name="outcomes" className="element">
+          <Outcomes />
+        </Element>
+        <Element name="strategy" className="element">
+          <Strategy />
+        </Element>
+        <Element name="team" className="element">
+          <Team />
+        </Element>
+        <Element name="process" className="element">
+          <Phases />
+        </Element>
+        <Element name="contact" className="element">
+          <Contact />
+        </Element>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
