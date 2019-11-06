71f684f1866e7f5ca157f5bbff43155aa102e13d
Patrick Bower <bower.patrick@gmail.com>
2019-11-06 17:39:18 +0000

    New images and layout changes to Outcomes in Phases
    Three new SVGs for each Phase
    Using Grav classes to layout outcome bullets

--- src/components/phases.js
+++ src/components/phases.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ContentCard from "./contentCard";
-import TempBuildTestLearn from "../images/temp-build-test-learn.png";
-import AssumptionsMapSvg from "./assumptionsMapSvg";
-import CompileFindingsSvg from "./CompileFindingsSvg";
+// import TempBuildTestLearn from "../images/temp-build-test-learn.png";
+// import AssumptionsMapSvg from "./assumptionsMapSvg";
+import PhaseOne from "./phaseOne";
+import PhaseTwo from "./phaseTwo";
+import PhaseThree from "./phaseThree";
+// import CompileFindingsSvg from "./CompileFindingsSvg";
 
 const Phases = () => (
   <section className="grav-u-pt-xxl grav-u-pb-xxl">
@@ -50,19 +53,25 @@ const Phases = () => (
               </p>
             </ContentCard>
             <div>
-            <div className="c7-phase1-graphic">
-              <div className="c7-phase1-graphic__map">
-                <AssumptionsMapSvg />
+              <div className="c7-phase1-graphic">
+              <PhaseOne />
               </div>
             </div>
-            <h5>Outcomes</h5>
-                <ul>
-                  <li>Assumptions map; a living document for measuring what needs to be learnt</li>
-                  <li>Clearly defined and prioritised hypotheses</li>
-                  <li>Strategy for building the right experiments to test each hypothesis</li>
-                </ul>
-            </div>
           </div>
+          <ContentCard>
+          <h5>Outcomes</h5>
+          <ul className="grav-o-two-column">
+            <li>
+              Assumptions map; a living document for measuring what needs to
+              be learnt
+            </li>
+            <li>Clearly defined and prioritised hypotheses</li>
+            <li>
+              Strategy for building the right experiments to test each
+              hypothesis
+            </li>
+          </ul>
+        </ContentCard>
         </div>
       </div>
       <div className="c7-phase grav-u-pb-xl" data-gtm-track="phase-2">
@@ -94,26 +103,31 @@ const Phases = () => (
                 Rapid prototyping
               </h4>
               <p className="cardText">
-                Experiments are built for users to test and designed
-                to determine the validity of the hypothesis. This is usually a prototype
-                containing prioritised features of the overall proposition.
+                Experiments are built for users to test and designed to
+                determine the validity of the hypothesis. This is usually a
+                prototype containing prioritised features of the overall
+                proposition.
               </p>
               <p className="cardText">
-                We replace lengthy, speculative research; instead giving users
-                a realistic view of what the proposition actually means to them.
+                We replace lengthy, speculative research; instead giving users a
+                realistic view of what the proposition actually means to them.
               </p>
             </ContentCard>
             <div className="c7-graphic">
-              <img src={TempBuildTestLearn} alt="" />
-              <h5>Outcomes</h5>
-              <ul>
-                <li>Data-lead experiments tested with real users</li>
-                <li>Conclusive insight into the validity of the hypotheses</li>
-                <li>Crucial insight into what's desirable, viable and feasible</li>
-                <li>Typically, a prototype containing the key features</li>
-              </ul>
+              <PhaseTwo />
             </div>
           </div>
+          <ContentCard>
+            <h5>Outcomes</h5>
+            <ul className="grav-o-two-column">
+              <li>Data-lead experiments tested with real users</li>
+              <li>Conclusive insight into the validity of the hypotheses</li>
+              <li>
+                Crucial insight into what's desirable, viable and feasible
+              </li>
+              <li>Typically, a prototype containing the key features</li>
+            </ul>
+          </ContentCard>
         </div>
       </div>
       <div className="c7-phase grav-u-pb-xl" data-gtm-track="phase-3">
@@ -145,28 +159,38 @@ const Phases = () => (
               </h4>
               <p className="cardText">
                 Findings are formatted to make it easy to communicate them to
-                all stakeholders. This includes presenting the data gathered during user
-                testing; demonstrating the validated hypotheses.
+                all stakeholders. This includes presenting the data gathered
+                during user testing; demonstrating the validated hypotheses.
               </p>
               <p>
                 We ensure that a strategy for moving forward is made clear. This
                 could include providing a well-defined backlog, technical
-                decisions, and continued support for scaling the proposal. In the
-                case of uncovering pivot points or no-gos, we reinforce this as
-                a positive outcome by identifying further experiments or
+                decisions, and continued support for scaling the proposal. In
+                the case of uncovering pivot points or no-gos, we reinforce this
+                as a positive outcome by identifying further experiments or
                 propositions.
               </p>
             </ContentCard>
             <div>
-              <CompileFindingsSvg />
-              <h5>Outcomes</h5>
-              <ul>
-                <li>A well defined backlog of activities that should happen next</li>
-                <li>Clear, insightful reports detailing how risk has been reduced</li>
-                <li>A strategy for moving forward or pivoting in a new direction</li>
-              </ul>
+            <PhaseThree />
             </div>
           </div>
+        </div>
+        <div className="grav-o-full-bleed__content">
+          <ContentCard>
+            <h5>Outcomes</h5>
+            <ul className="grav-o-two-column">
+              <li>
+                A well defined backlog of activities that should happen next
+              </li>
+              <li>
+                Clear, insightful reports detailing how risk has been reduced
+              </li>
+              <li>
+                A strategy for moving forward or pivoting in a new direction
+              </li>
+            </ul>
+          </ContentCard>
         </div>
       </div>
     </div>
