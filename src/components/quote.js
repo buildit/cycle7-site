b9e900e4607c9c8277679d9948d47c2476590e0e
Dan Bull <daniel.bull@wipro.com>
2019-11-15 15:52:14 +0000

    Adding quote and moving scss

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PatrickBower from "../images/patrickbower.jpg";
+import DanielBull from "../images/danielbull.jpg";
 
 const Quote = () => (
   <section>
@@ -12,6 +13,16 @@ const Quote = () => (
             feasibility for ideas without any of the usual ambiguity.
           </p>
           <p className="quote__name">Patrick Bower, Cycle 7 practitioner.</p>
+        </div>
+      </div>
+      <div className="quote">
+        <img src={DanielBull} alt="Daniel Bull" className="quote__person" />
+        <div className="quote__speech">
+          <p className="grav-u-font-size-plus-3">
+            Cycle 7 allows us to focus on what really matters to users and iterate quickly, based
+            on their feedback.
+          </p>
+          <p className="quote__name">Daniel Bull, Cycle 7 practitioner.</p>
         </div>
       </div>
     </div>
