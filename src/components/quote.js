40f50e5604af7c077f79c399b6b1b86a2352426e
Patrick Bower <bower.patrick@gmail.com>
2019-11-15 12:33:21 +0000

    So good I put my name on it

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,11 +1,19 @@
 import React from "react";
+import PatrickBower from "../images/patrickbower.jpg";
 
 const Quote = () => (
-  <section className="quote">
+  <section>
     <div className="grav-o-full-bleed__content">
-      <p className="grav-u-font-size-plus-3">
-        "Using Cycle 7 we can measure the desirability, viability and feasibility for ideas without ambiguity."
-      </p>
+      <div className="quote">
+        <img src={PatrickBower} alt="Patrick Bower" className="quote__person" />
+        <div className="quote__speech">
+          <p className="grav-u-font-size-plus-3">
+            Using Cycle 7 we can measure the desirability, viability and
+            feasibility for ideas without any of the usual ambiguity.
+          </p>
+          <p className="quote__name">Patrick Bower, Cycle 7 practitioner.</p>
+        </div>
+      </div>
     </div>
   </section>
 );
